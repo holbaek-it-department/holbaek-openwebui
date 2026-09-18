63eb3cbeb7f94eafdc33ef47c528bb3f6fd69e0a
Casper Nymark <cnymark@live.dk>
2026-09-18 13:37:35 +0200

    updated write_hej.yaml

--- cron-test/write_hej.py
+++ cron-test/write_hej.py
@@ -7,3 +7,6 @@ with file_path.open("a", encoding="utf-8") as file:
     file.write("hej\n")
 
 print(f"{datetime.now().isoformat()} - skrev 'hej' til {file_path}")
+
+print("\nFilens nuværende indhold:")
+print(file_path.read_text(encoding="utf-8"))
