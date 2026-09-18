1b8a0d7bedc28733cd5c55e24815bc223d07005b
Casper Nymark <cnymark@live.dk>
2026-09-18 13:15:37 +0200

    added test for cron job

---
+++ cron-test/write_hej.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from datetime import datetime
+
+file_path = Path("/data/test.txt")
+
+with file_path.open("a", encoding="utf-8") as file:
+    file.write("hej\n")
+
+print(f"{datetime.now().isoformat()} - skrev 'hej' til {file_path}")
